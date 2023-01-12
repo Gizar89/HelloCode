240f2e7f422a5fe2b03ce632108df001d55d14aa
Gizar <rahimbaev.gizar@mail.ru>
2023-01-12 19:49:02 +0500

    Example005_if_Else

---
+++ Example005_if_Elso/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маркиза!")
+{
+    Console.WriteLine ("Ура - это же Маркиза");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
